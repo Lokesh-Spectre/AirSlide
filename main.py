6481ab02ac3f3e723b4b8e9c8ac92a0bc01c898a
lokesh-emblock <lokeshwaran@emblocktech.com>
2025-03-26 18:28:38 +0530

    hand 3d and full body hand detection

--- main.py
+++ main.py
@@ -1,38 +1,43 @@
-import cv2
 import mediapipe as mp
-import numpy as np
+import cv2
 
-# Initialize MediaPipe Pose module
-mp_pose = mp.solutions.pose
 mp_drawing = mp.solutions.drawing_utils
+mp_holistic = mp.solutions.holistic
 
-# Start capturing video
 cap = cv2.VideoCapture(0)
-
-# Initialize Pose model
-with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+# Initiate holistic model
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    
     while cap.isOpened():
         ret, frame = cap.read()
-        if not ret:
-            break
         
-        # Convert the image to RGB
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # Recolor Feed
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # Make Detections
+        results = holistic.process(image)
+        # print(results.face_landmarks)
         
-        # Process the frame with Pose model
-        results = pose.process(rgb_frame)
+        # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
+        
+        # Recolor image back to BGR for rendering
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        # Draw face landmarks
+        # mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
+        
+        # Right hand
+        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
 
-        # Draw pose landmarks
-        if results.pose_landmarks:
-            mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+        # Left Hand
+        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
 
-        # Display the frame
-        cv2.imshow('Full-Body Pose Estimation', frame)
+        # Pose Detections
+        # mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+                        
+        cv2.imshow('Raw Webcam Feed', image)
 
-        # Exit on 'q' key press
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
-# Release resources
 cap.release()
 cv2.destroyAllWindows()
